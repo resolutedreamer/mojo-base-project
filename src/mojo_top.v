8dc534ab495f4f3249bee4a15a8aca35e86d6a6c
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-11-25 01:17:35 -0800

    Configured LED to blink continuously

--- src/mojo_top.v
+++ src/mojo_top.v
@@ -27,6 +27,12 @@ assign spi_miso = 1'bz;
 assign avr_rx = 1'bz;
 assign spi_channel = 4'bzzzz;
 
-assign led = 8'b0;
+assign led[7:1] = 7'b0;
+
+blinker awesome_blinker (
+	.clk(clk),
+	.rst(rst),
+	.blink(led[0])
+);
 
 endmodule
