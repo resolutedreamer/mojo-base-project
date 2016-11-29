732cc4698cd7667d14257ebee3d20b3b63c7eac7
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-11-28 20:23:55 -0800

    Create pwm.v
    The module pwm will vary the duty cycle of the output based on the
    input.
    Connect 8 copies of the pwm module to the 8 leds on the board

--- src/mojo_top.v
+++ src/mojo_top.v
@@ -20,13 +20,60 @@ module mojo_top(
     input avr_rx_busy // AVR Rx buffer full
     );
 
-wire rst = ~rst_n; // make reset active high
+	wire rst = ~rst_n; // make reset active high
 
-// these signals should be high-z when not used
-assign spi_miso = 1'bz;
-assign avr_rx = 1'bz;
-assign spi_channel = 4'bzzzz;
-
-assign led = 8'b0;
-
+	// these signals should be high-z when not used
+	assign spi_miso = 1'bz;
+	assign avr_rx = 1'bz;
+	assign spi_channel = 4'bzzzz;
+	
+	pwm #(.CTR_LEN(3)) pwm_1 (
+		.rst(rst),
+		.clk(clk),
+		.compare(3'd0),
+		.pwm(led[0])
+	);
+	pwm #(.CTR_LEN(3)) pwm_2 (
+		.rst(rst),
+		.clk(clk),
+		.compare(3'd1),
+		.pwm(led[1])
+	);
+	pwm #(.CTR_LEN(3)) pwm_3 (
+		.rst(rst),
+		.clk(clk),
+		.compare(3'd2),
+		.pwm(led[2])
+	);
+	pwm #(.CTR_LEN(3)) pwm_4 (
+		.rst(rst),
+		.clk(clk),
+		.compare(3'd3),
+		.pwm(led[3])
+	);
+	pwm #(.CTR_LEN(3)) pwm_5 (
+		.rst(rst),
+		.clk(clk),
+		.compare(3'd4),
+		.pwm(led[4])
+	);
+	pwm #(.CTR_LEN(3)) pwm_6 (
+		.rst(rst),
+		.clk(clk),
+		.compare(3'd5),
+		.pwm(led[5])
+	);
+	pwm #(.CTR_LEN(3)) pwm_7 (
+		.rst(rst),
+		.clk(clk),
+		.compare(3'd6),
+		.pwm(led[6])
+	);
+	pwm #(.CTR_LEN(3)) pwm_8 (
+		.rst(rst),
+		.clk(clk),
+		.compare(3'd7),
+		.pwm(led[7])
+	);
+  
 endmodule
